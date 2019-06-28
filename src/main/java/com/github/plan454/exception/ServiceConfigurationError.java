485f785af856cbf5da3b2c249ec96bdfc3d8b4cc
zhanyue <zhanyue@zhanyuedeMacBook-Pro.local>
2019-06-28 19:50:08 +0800

    init

---
+++ src/main/java/com/github/plan454/exception/ServiceConfigurationError.java
@@ -0,0 +1,8 @@
+package com.github.plan454.exception;
+
+public class ServiceConfigurationError extends RuntimeException {
+
+    public ServiceConfigurationError(Throwable cause) {
+        super(cause);
+    }
+}
